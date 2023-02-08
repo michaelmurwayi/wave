96bb32f1dcc7c2314f587fa7970762151d241959
Michael Murwayi <mikemurwayi7@gmail.com>
2023-02-08 14:17:33 +0300

    ft-view: Adding route to donate view

--- app/web/urls.py
+++ app/web/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import HomeView
+from .views import HomeView, DonateView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home")
+    path('', HomeView.as_view(), name="home"),
+    path('donate/', DonateView.as_view(), name="donate")
 ]
